fdedf3f2d29d8a80e1fe6f02519b925b9111047a
luis-c2255 <lmodesto63@gmail.com>
2026-02-21 22:44:27 +0100

    up

--- pages/5_💹_Netflix_Stock_Analysis_Dashboard.py
+++ pages/5_💹_Netflix_Stock_Analysis_Dashboard.py
@@ -621,7 +621,7 @@ with col2:
     st.markdown(
         Components.metric_card(
         title="Daily_Volatility_Pct",
-        value=f"df['Daily_Return'].std()",
+        value=f"{df['Daily_Return'].std()}",
         delta="PCT",
         card_type="info"
     ), unsafe_allow_html=True)
